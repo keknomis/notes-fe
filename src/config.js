2f5551aa6f02f3d154946e410956a395fa01ab85
simon <simon.serbetar1@gmail.com>
2025-04-22 08:16:54 +0200

    resolve deploy issue

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-// src/config.js
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5001";
-
+// const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5001";
+const API_BASE_URL = "https://tippy-todo.onrender.com";
 export default API_BASE_URL;
