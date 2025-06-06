e0bd2a48ae20a4ff0f0a331ca58e7a2a14c52e58
simon <simon.serbetar1@gmail.com>
2025-06-06 12:50:26 +0200

    updated config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-// const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5001";
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5001";
 // const API_BASE_URL = "https://tippy-todo.onrender.com";
-const API_BASE_URL = "http://localhost:5001";
+// const API_BASE_URL = "http://localhost:5001";
 export default API_BASE_URL;
