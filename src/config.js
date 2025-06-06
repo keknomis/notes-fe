ec53527a66be2771191bb122c24a6114d8a2310a
simon <simon.serbetar1@gmail.com>
2025-06-06 12:59:18 +0200

    changed based url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5001";
+// const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5001";
+const API_BASE_URL = "https://notes-api-l8p4.onrender.com";
 export default API_BASE_URL;
